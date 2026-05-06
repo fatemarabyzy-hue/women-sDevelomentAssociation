e19f4f7a3ffc69fa2ee4d3824bb7b84721b8fa13
fatemarabyzy-hue <fatemarabyzy@gmail.com>
2026-05-06 03:03:05 +0300

    Add files via upload

---
+++ lib/screens/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'screens/home_screen.dart';
+import 'package:wda/constant.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'جمعية تنمية المرأة',
+      debugShowCheckedModeBanner: false,
+      locale: const Locale('ar', 'SA'),
+      supportedLocales: const [
+        Locale('ar', 'SA'),
+      ],
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      theme: ThemeData(
+        primaryColor: AppColors.primaryGreen,
+        scaffoldBackgroundColor: AppColors.background,
+        fontFamily: 'Cairo',
+        appBarTheme: const AppBarTheme(
+          backgroundColor: AppColors.primaryGreen,
+          elevation: 0,
+          centerTitle: true,
+        ),
+        floatingActionButtonTheme: const FloatingActionButtonThemeData(
+          backgroundColor: AppColors.primaryGreen,
+        ),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
